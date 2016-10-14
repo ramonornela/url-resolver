b58757087263f76c7f6e6590feed3bdeaf6037c9
ramonornela <ramon.ornela@gmail.com>
2016-10-13 23:04:13 -0300

    refactor(metadata-json): eliminando referencia do ionic

--- src/metadata/json.ts
+++ src/metadata/json.ts
@@ -2,7 +2,6 @@ import { MetadataBase } from './metadata-base';
 import { Injectable, NgZone } from '@angular/core';
 import { Json } from '@angular/core/src/facade/lang';
 import { BrowserXhr } from '@angular/http';
-import { isBlank } from 'ionic-angular/util/util';
 
 @Injectable()
 export class JsonMetadata extends MetadataBase {
@@ -13,7 +12,7 @@ export class JsonMetadata extends MetadataBase {
 
   getData() {
 
-    if (isBlank(this._data)) {
+    if (this._data === null || this._data === undefined) {
 
       this.zone.runOutsideAngular(() => {
         let _xhr = this.xhr.build();
