7af19976f49493fa237a5c28aec7ba7d8304e321
ramonornela <ramon.ornela@gmail.com>
2016-10-17 11:20:01 -0200

    chore(): improvements readme and lint

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var packageJson = require(path.join('..', 'package.json'));
 var readme = fs.readFileSync(path.join(process.cwd(), 'README.md'));
+var license = fs.readFileSync(path.join(process.cwd(), 'LICENSE'));
 
 if (! packageJson) {
   throw new Error('Can\'t find package json');
@@ -13,8 +14,13 @@ if (! readme) {
   throw new Error('Can\'t find README.md');
 }
 
+if (! license) {
+  throw new Error('Can\'t find LICENSE');
+}
+
 delete packageJson.devDependencies;
 delete packageJson.scripts;
 
 fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
 fs.writeFile('./dist/REAME.md', readme);
+fs.writeFile('./dist/LICENSE', license);
