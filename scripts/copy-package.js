d6d61d94fd13e26e39b267a57056c6c8eeff8007
ramonornela <ramon.ornela@gmail.com>
2016-10-17 10:37:49 -0200

    chore(npm): copia do readme para publicacao no npm

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
+var process = require('process');
+var path = require('path');
 
-var packageJson = require('../package.json');
+var packageJson = require(path.join('..', 'package.json'));
+var readme = fs.readFileSync(path.join(process.cwd(), 'README.md'));
+
 if (! packageJson) {
   throw new Error('Can\'t find package json');
+}
+
+if (! readme) {
+  throw new Error('Can\'t find README.md');
 }
 
 delete packageJson.devDependencies;
 delete packageJson.scripts;
 
 fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
+fs.writeFile('./dist/REAME.md', readme);
