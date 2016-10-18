5e47e2eb235e2b6abedae74f6af6f883d4193fc9
ramonornela <ramon.ornela@gmail.com>
2016-10-18 17:23:13 -0200

    chore(): delecao de arquivos desnessarios para publicacao

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -3,24 +3,12 @@ var process = require('process');
 var path = require('path');
 
 var packageJson = require(path.join('..', 'package.json'));
-var readme = fs.readFileSync(path.join(process.cwd(), 'README.md'));
-var license = fs.readFileSync(path.join(process.cwd(), 'LICENSE'));
 
 if (! packageJson) {
   throw new Error('Can\'t find package json');
-}
-
-if (! readme) {
-  throw new Error('Can\'t find README.md');
-}
-
-if (! license) {
-  throw new Error('Can\'t find LICENSE');
 }
 
 delete packageJson.devDependencies;
 delete packageJson.scripts;
 
 fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
-fs.writeFile('./dist/REAME.md', readme);
-fs.writeFile('./dist/LICENSE', license);
