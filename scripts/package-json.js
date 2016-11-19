0725a42c1506bd2ffd3ffe4eddf0b9dcb6795137
ramonornela <ramon.ornela@gmail.com>
2016-11-19 21:52:30 -0200

    chore(devOps): improvements scripts publish npm

--- scripts/package-json.js
+++ scripts/package-json.js
@@ -1,31 +1,44 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
 var process = require('process');
 var path = require('path');
 var process = require('process');
 
 var originalPath = path.join(process.cwd(), 'package.json');
-var tempPath = path.join(process.cwd(), 'package_tmp.json');
+var publishPath = path.join(__dirname, 'package-publish.json');
+var backupPath = path.join(process.cwd(), 'package-orig.json');
 
 var Package = {
 
   prepare: function() {
 	  
     var packageJson = require(originalPath);
-    var readme = fs.readFileSync(path.join(process.cwd(), 'README.md'));
+    var packagePublish = require(publishPath);
 
     if (! packageJson) {
       throw new Error('Can\'t find package json');
     }
 
-    delete packageJson.devDependencies;
-    delete packageJson.dependencies;
+    if (! packagePublish) {
+      throw new Error('Can\'t find package-publish json');
+    }
 
-    fs.renameSync(originalPath, tempPath);
+    Object.assign(packageJson, packagePublish);
+
+    // delete keys null
+    for (var config in packageJson) {
+      if (!packageJson[config]) {
+        delete packageJson[config];
+      }
+    }
+
+    fs.renameSync(originalPath, backupPath);
     fs.writeFileSync(originalPath, JSON.stringify(packageJson, null, 2));	
   },
 
   restore: function() {
-    fs.renameSync(tempPath, originalPath);
+    fs.renameSync(backupPath, originalPath);
   }
 }
 
