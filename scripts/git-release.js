93fd1e1def19f6cd36ceb78743e0b17bdd3e3753
ramonornela <ramon.ornela@gmail.com>
2016-12-12 16:53:07 -0200

    chore(devOps): improvement script publish tag

--- scripts/git-release.js
+++ scripts/git-release.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-var changelog = require('conventional-changelog');
+var execSync = require('child_process').execSync;
+var changelogCommand = './node_modules/.bin/conventional-changelog -p angular';
+var changelogContent = execSync(changelogCommand).toString();
+
 var GithubApi = require('github');
 var packageJSON = require('../package.json');
-var through = require('through2');
 var token = process.argv[2] ? process.argv[2] : null;
 
 var github = new GithubApi({
@@ -15,31 +17,15 @@ github.authenticate({
   token: token || process.env.GH_TOKEN
 });
 
-function publish(body) {
-  github.releases.createRelease({
-    owner: 'ramonornela',
-    repo: 'url-resolver',
-    target_commitish: 'master',
-    tag_name: 'v' + packageJSON.version,
-    name: packageJSON.version,
-    body: body,
-    prerelease: false
-  }, function(err, data) {
-    if (err) console.log('error: ' + err);
-    console.log(data);
-  });
-}
-
-var generating = false;
-
-changelog({
-  preset: 'angular'
-})
-.pipe(through.obj(function(file) {
-  generating = true;
-  publish(file.toString());
-}));
-
-if (!generating) {
-  publish();
-}
+github.releases.createRelease({
+  owner: 'ramonornela',
+  repo: 'http',
+  target_commitish: 'master',
+  tag_name: 'v' + packageJSON.version,
+  name: packageJSON.version,
+  body: changelogContent,
+  prerelease: false
+}, function(err, data) {
+  if (err) console.log('error: ' + err);
+  console.log(data);
+});
